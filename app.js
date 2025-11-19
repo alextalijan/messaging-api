6634371aad9d3c049359efd7679d198d055adee6
alextalijan <acatalijan@icloud.com>
2025-11-19 15:50:15 -0600

    feat: configure passport authentication

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
+
+// https://github.com/kleydon/prisma-session-store#readme
+// Set up express sessions with prisma store
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
 // Import routers
 const usersRouter = require('./routes/usersRouter');
 
