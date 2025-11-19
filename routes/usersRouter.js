4997d3a863810af0467663de58e73844f14470d7
alextalijan <acatalijan@icloud.com>
2025-11-19 16:26:28 -0600

    feat: add necessary routes for reading and sending messages in chat

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -7,5 +7,15 @@ const controller = require('../controllers/usersController');
 router.get('/:username', controller.getUser);
 router.get('/:username/chats', isAuthenticated, controller.getUserChats);
 router.post('/:username/chats', isAuthenticated, controller.createUserChat);
+router.get(
+  '/:username/chats/:chatId',
+  isAuthenticated,
+  controller.getChatMessages
+);
+router.post(
+  '/:username/chats/:chatId',
+  isAuthenticated,
+  controller.sendChatMessage
+);
 
 module.exports = router;
